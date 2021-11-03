e25df27999ba38e32d3b68a76e7ede71055cb874
ludeerin <89855207+ludeerin@users.noreply.github.com>
2021-11-02 20:24:37 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,20 @@
+var video = document.querySelector("#player1")
+
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
+
+//use html videos DOM on week 7 materials
+//platbackRate for slowdown and speedup
